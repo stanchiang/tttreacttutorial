f7ca1d98f993d76cbd1401798f8e793da7b7f42d
stanchiang <stanchiang23@gmail.com>
2018-07-01 20:38:04 -0500

    taking turns

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -21,8 +21,8 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X':'O';
+    this.setState({squares: squares, xIsNext: !this.state.xIsNext});
   }
 
   renderSquare(i) {
@@ -30,7 +30,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X':'O');
 
     return (
       <div>
